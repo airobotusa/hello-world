48afbc1c5b13098ea06c4fc55c2033b807fa0650
airobot <airobotusa@gmail.com>
2018-01-25 23:19:34 -0500

    222

--- swap.c
+++ swap.c
@@ -15,7 +15,8 @@ int main(void)
 	// swapping is done
 	// merge conflicts
 	// merge made conflicts on purpose
-    	
+ 	// merge 2 for conflicts 222
+
     printf("x is now %i\n", x);
     printf("y is now %i\n", y);
 }
